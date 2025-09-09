52e3f40f11f2c9f4324af8c1f1fa722525645a96
Maeli <maeli.palharini@hotmail.com>
2025-09-09 11:43:33 -0300

    ✨feat: Etapa 2: Rotas configuradas e Navbar funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import Navbar from "./components/navbar/Navbar.tsx";
 import Footer from "./components/footer/Footer.tsx";
 import Home from "./pages/Home.tsx";
+import {Route, Routes} from "react-router-dom";
 
-function App() {
+export default function App() {
     return (
         <div className="min-h-screen flex flex-col">
             <Navbar />
-            <div className="flex-1">
-                <Home />
-            </div>
+            <main className="flex-1">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/categorias" element={<h1 className="p-8 text-2xl">Página de Categorias</h1>} />
+                    <Route path="/categorias/novo" element={<h1 className="p-8 text-2xl">Cadastrar Nova Categoria</h1>} />
+                    <Route path="*" element={<h1 className="p-8 text-2xl">Página não encontrada 😢</h1>} />
+                </Routes>
+            </main>
             <Footer />
         </div>
     );
 }
-
-export default App;
